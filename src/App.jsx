b20e7b03a4acbc2430b5993e8dd0d8080d448e4f
MaDGiiRL <sofiavidotto8@gmail.com>
2026-02-28 21:35:03 +0100

    fix mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Book3D from "./Book3D.jsx";
 
 export default function App() {
-  return <Book3D totalPages={16} ext="webp" title="Documento" />;
+  return <Book3D totalPages={15} ext="webp" title="Menù Digitale" />;
   // se hai png: ext="png"
 }
